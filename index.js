7409eab194e7cede9f630a62037f3f112f77b127
antipro <antipro@163.com>
2017-12-02 16:12:57 +0800

    123

--- index.js
+++ index.js
@@ -101,24 +101,34 @@ app.get('/api/projects/:pid', (req, res) => {
 
 const sockets = new Object()
 
+/**
+ * Timeout对象集合，每个task的id对应一组timeout
+ */
 let timers = new Object()
-
-let updateTimers = (id) => {
+/**
+ * 更新id对应的所有timeout
+ * @param id 
+ */
+function updateTimers (id) {
+  console.log('update timer of task #%d', id)
   if (timers[id]) {
     timers[id].forEach(timer => {
       clearTimeout(timer)
     })
   }
   timers[id] = new Array()
-  let sql = `SELECT a.id, a.content, b.uid, DATE_FORMAT(a.notify_date, '%Y-%m-%d') AS notify_date, DATE_FORMAT(IFNULL(a.notify_time, c.notify_time), '%H:%i') AS notify_time 
+  let sql = `SELECT a.id, a.content, b.uid, DATE_FORMAT(a.notify_date, \'%Y-%m-%d\') AS notify_date, DATE_FORMAT(IFNULL(a.notify_time, c.notify_time), \'%H:%i\') AS notify_time 
     FROM tasks a, projects b, users c WHERE a.id = ? AND a.pid = b.id AND b.uid = c.id AND a.state = 0 AND a.notify_date >= CURRENT_DATE
     UNION
-    SELECT a.id, a.content, b.uid, DATE_FORMAT(a.notify_date, '%Y-%m-%d') AS notify_date, DATE_FORMAT(IFNULL(a.notify_time, c.notify_time), '%H:%i') AS notify_time 
-    FROM tasks a, shares b, users c WHERE a.id = ? b.uid = c.id AND a.pid = b.pid AND a.state = 0 AND a.notify_date >= CURRENT_DATE`
+    SELECT a.id, a.content, b.uid, DATE_FORMAT(a.notify_date, \'%Y-%m-%d\') AS notify_date, DATE_FORMAT(IFNULL(a.notify_time, c.notify_time), \'%H:%i\') AS notify_time 
+    FROM tasks a, shares b, users c WHERE a.id = ? AND b.uid = c.id AND a.pid = b.pid AND a.state = 0 AND a.notify_date >= CURRENT_DATE`
   pool.promise(sql, [ id, id ]).then(results => {
     results.forEach(task => {
       let current_time = Date.now()
       let future_time = Date.parse(task.notify_date + ' ' + task.notify_time + ':00')
+      if (future_time < current_time) {
+        return
+      }
       let timer = setTimeout(() => {
         console.log('push task #%d to user #%d', task.id, task.uid)
         if (sockets[task.uid]) {
@@ -128,11 +138,12 @@ let updateTimers = (id) => {
         }
       }, future_time - current_time)
       timers[id].push(timer)
-    }).catch((err) => {
-      console.error(err)
-      socket.emit('error event', '查询推送出错')
     })
+  }).catch((err) => {
+    console.error(err)
+    socket.emit('error event', '查询推送出错')
   })
+  console.log(timers)
 }
 let sql = `SELECT id FROM tasks WHERE state = 0 AND notify_date >= CURRENT_DATE`
 pool.promise(sql).then(results => {

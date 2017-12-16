4df04ee168e451a2eec4a75df3c814b50c0ce53a
antipro <antipro@163.com>
2017-12-16 08:38:28 +0800

    update sql

--- sharelist.sql
+++ sharelist.sql
@@ -17,7 +17,6 @@ CREATE DATABASE IF NOT EXISTS `sharelist` /*!40100 DEFAULT CHARACTER SET utf8 */
 USE `sharelist`;
 
 -- Dumping structure for table sharelist.projects
-DROP TABLE IF EXISTS `projects`;
 CREATE TABLE IF NOT EXISTS `projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
@@ -26,11 +25,10 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `editable` char(1) NOT NULL DEFAULT 'Y',
   `state` tinyint(2) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table sharelist.shares
-DROP TABLE IF EXISTS `shares`;
 CREATE TABLE IF NOT EXISTS `shares` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) NOT NULL,
@@ -38,11 +36,10 @@ CREATE TABLE IF NOT EXISTS `shares` (
   `control` char(3) NOT NULL DEFAULT 'R--',
   `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table sharelist.tasks
-DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
@@ -53,21 +50,32 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `notify_date` date DEFAULT NULL,
   `notify_time` time DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=251 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table sharelist.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tel` bigint(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `pwd` varchar(50) DEFAULT NULL,
   `token` varchar(50) DEFAULT NULL,
   `notify_time` time NOT NULL DEFAULT '09:00:00',
+  `locale` varchar(50) DEFAULT NULL,
   `ctime` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+-- Dumping structure for table sharelist.verifycodes
+CREATE TABLE IF NOT EXISTS `verifycodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `uuid` varchar(50) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `expire` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;

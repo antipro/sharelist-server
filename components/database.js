e79b638ec6b41b5cbefbf8faf7dca51d1ae3c1b3
antipro <antipro@163.com>
2026-01-06 16:35:55 +0800

    refactor: streamline database and email configuration handling

--- components/database.js
+++ components/database.js
@@ -1,27 +1,11 @@
 /**
  * database pool function
  */
-if (process.env['DYNO'] !== undefined) { // heroku environment
-  var config = {
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
-  }
-} else { // indepency environment
-  try {
-    var config = require('./config.js').config
-  } catch (error) {
-    console.log('Please create config.js file.')
-    console.log('Content like this:')
-    console.log(`exports.config = {
-      host: 'host',
-      user: 'user',
-      password: 'password',
-      database: 'database'
-    }`)
-    process.exit(-1)
-  }
+var config = {
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
 }
 // init connnection pool of database
 const pool = require('mysql').createPool({
--- components/mail.js
+++ components/mail.js
@@ -1,58 +1,31 @@
 /**
  * sendmail function
  */
-if (process.env['DYNO'] !== undefined) { // heroku environment
-  var config = {
-    mailuser: process.env.mailuser,
-    mailpwd: process.env.mailpwd,
-    smtpserver: process.env.smtpserver,
-    smtpport: parseInt(process.env.smtpport)
-  }
-  const sgMail = require('@sendgrid/mail')
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  exports.sendmail = function (mail) {
-    return sgMail.send({
-      to: mail.to,
-      from: `${mail.mailsender} <${config.mailuser}>`,
-      subject: mail.subject,
-      text: mail.text,
-      html: mail.html,
-    })
-  }
-} else { // indepency environment
-  try {
-    var config = require('./config.js').config
-  } catch (error) {
-    console.log('Please create config.js file.')
-    console.log('Content like this:')
-    console.log(`exports.config = {
-      mailuser: 'mailuser',
-      mailpwd: 'mailpwd',
-      smtpserver: 'smtpserver',
-      smtpport: smtpport
-    }`)
-    process.exit(-1)
-  }
-  const email = require('emailjs')
-  const util = require('util')
-  exports.sendmail = function (mail) {
-    var server = email.server.connect({
-      user: config.mailuser,
-      password: config.mailpwd,
-      host: config.smtpserver,
-      port: config.smtpport,
-      ssl: true
-    });
-    server.promise = util.promisify(server.send)
-    return server.promise({
-      text: mail.text,
-      from: `${mail.mailsender} <${config.mailuser}>`,
-      to: mail.to,
-      subject: mail.subject,
-      attachment:
-      [
-        { data: mail.html, alternative: true }
-      ]
-    })
-  }
+var config = {
+  mailuser: process.env.mailuser,
+  mailpwd: process.env.mailpwd,
+  smtpserver: process.env.smtpserver,
+  smtpport: parseInt(process.env.smtpport)
+}
+const email = require('emailjs')
+const util = require('util')
+exports.sendmail = function (mail) {
+  var server = email.server.connect({
+    user: config.mailuser,
+    password: config.mailpwd,
+    host: config.smtpserver,
+    port: config.smtpport,
+    ssl: true
+  });
+  server.promise = util.promisify(server.send)
+  return server.promise({
+    text: mail.text,
+    from: `${mail.mailsender} <${config.mailuser}>`,
+    to: mail.to,
+    subject: mail.subject,
+    attachment:
+    [
+      { data: mail.html, alternative: true }
+    ]
+  })
 }
